179762c81acfd4464d43700083b85c4095ea394d
Martin Schilling <martin@mutz.it>
2014-03-24 15:42:03 +0100

    Initial Commit

---
+++ dottube/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace dottube
+{
+    partial class formMainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMainForm));
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageYouTube = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBoxVideoInformation = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelVideoDescription = new System.Windows.Forms.Label();
+            this.labelShowFileName = new System.Windows.Forms.Label();
+            this.labelShowFormatID = new System.Windows.Forms.Label();
+            this.labelShowVideoURL = new System.Windows.Forms.Label();
+            this.labelShowVideoTitle = new System.Windows.Forms.Label();
+            this.labelShowVideoID = new System.Windows.Forms.Label();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.labelFormatID = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelVideoTitle = new System.Windows.Forms.Label();
+            this.labelVideoID = new System.Windows.Forms.Label();
+            this.labelURL = new System.Windows.Forms.Label();
+            this.buttonGetInfo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageYouTube.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBoxVideoInformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageYouTube);
+            this.tabControlMain.Controls.Add(this.tabPage2);
+            resources.ApplyResources(this.tabControlMain, "tabControlMain");
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            // 
+            // tabPageYouTube
+            // 
+            this.tabPageYouTube.Controls.Add(this.groupBox2);
+            resources.ApplyResources(this.tabPageYouTube, "tabPageYouTube");
+            this.tabPageYouTube.Name = "tabPageYouTube";
+            this.tabPageYouTube.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBoxVideoInformation);
+            this.groupBox2.Controls.Add(this.labelURL);
+            this.groupBox2.Controls.Add(this.buttonGetInfo);
+            this.groupBox2.Controls.Add(this.textBox1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBoxVideoInformation
+            // 
+            this.groupBoxVideoInformation.Controls.Add(this.label2);
+            this.groupBoxVideoInformation.Controls.Add(this.labelVideoDescription);
+            this.groupBoxVideoInformation.Controls.Add(this.labelShowFileName);
+            this.groupBoxVideoInformation.Controls.Add(this.labelShowFormatID);
+            this.groupBoxVideoInformation.Controls.Add(this.labelShowVideoURL);
+            this.groupBoxVideoInformation.Controls.Add(this.labelShowVideoTitle);
+            this.groupBoxVideoInformation.Controls.Add(this.labelShowVideoID);
+            this.groupBoxVideoInformation.Controls.Add(this.labelFileName);
+            this.groupBoxVideoInformation.Controls.Add(this.labelFormatID);
+            this.groupBoxVideoInformation.Controls.Add(this.label4);
+            this.groupBoxVideoInformation.Controls.Add(this.labelVideoTitle);
+            this.groupBoxVideoInformation.Controls.Add(this.labelVideoID);
+            resources.ApplyResources(this.groupBoxVideoInformation, "groupBoxVideoInformation");
+            this.groupBoxVideoInformation.Name = "groupBoxVideoInformation";
+            this.groupBoxVideoInformation.TabStop = false;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // labelVideoDescription
+            // 
+            resources.ApplyResources(this.labelVideoDescription, "labelVideoDescription");
+            this.labelVideoDescription.Name = "labelVideoDescription";
+            // 
+            // labelShowFileName
+            // 
+            resources.ApplyResources(this.labelShowFileName, "labelShowFileName");
+            this.labelShowFileName.Name = "labelShowFileName";
+            // 
+            // labelShowFormatID
+            // 
+            resources.ApplyResources(this.labelShowFormatID, "labelShowFormatID");
+            this.labelShowFormatID.Name = "labelShowFormatID";
+            // 
+            // labelShowVideoURL
+            // 
+            resources.ApplyResources(this.labelShowVideoURL, "labelShowVideoURL");
+            this.labelShowVideoURL.Name = "labelShowVideoURL";
+            // 
+            // labelShowVideoTitle
+            // 
+            resources.ApplyResources(this.labelShowVideoTitle, "labelShowVideoTitle");
+            this.labelShowVideoTitle.Name = "labelShowVideoTitle";
+            // 
+            // labelShowVideoID
+            // 
+            resources.ApplyResources(this.labelShowVideoID, "labelShowVideoID");
+            this.labelShowVideoID.Name = "labelShowVideoID";
+            // 
+            // labelFileName
+            // 
+            resources.ApplyResources(this.labelFileName, "labelFileName");
+            this.labelFileName.Name = "labelFileName";
+            // 
+            // labelFormatID
+            // 
+            resources.ApplyResources(this.labelFormatID, "labelFormatID");
+            this.labelFormatID.Name = "labelFormatID";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // labelVideoTitle
+            // 
+            resources.ApplyResources(this.labelVideoTitle, "labelVideoTitle");
+            this.labelVideoTitle.Name = "labelVideoTitle";
+            // 
+            // labelVideoID
+            // 
+            resources.ApplyResources(this.labelVideoID, "labelVideoID");
+            this.labelVideoID.Name = "labelVideoID";
+            // 
+            // labelURL
+            // 
+            resources.ApplyResources(this.labelURL, "labelURL");
+            this.labelURL.Name = "labelURL";
+            // 
+            // buttonGetInfo
+            // 
+            resources.ApplyResources(this.buttonGetInfo, "buttonGetInfo");
+            this.buttonGetInfo.Name = "buttonGetInfo";
+            this.buttonGetInfo.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // formMainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabControlMain);
+            this.Name = "formMainForm";
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageYouTube.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBoxVideoInformation.ResumeLayout(false);
+            this.groupBoxVideoInformation.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPageYouTube;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBoxVideoInformation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelVideoDescription;
+        private System.Windows.Forms.Label labelShowFileName;
+        private System.Windows.Forms.Label labelShowFormatID;
+        private System.Windows.Forms.Label labelShowVideoURL;
+        private System.Windows.Forms.Label labelShowVideoTitle;
+        private System.Windows.Forms.Label labelShowVideoID;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.Label labelFormatID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelVideoTitle;
+        private System.Windows.Forms.Label labelVideoID;
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.Button buttonGetInfo;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ dottube/Form1.cs
@@ -0,0 +1,15 @@
+﻿using log4net;
+using System.Windows.Forms;
+
+namespace dottube
+{
+    public partial class formMainForm : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public formMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
