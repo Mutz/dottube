cef1905dc097d9d8ce62ebda64b05e51e5b04491
Martin Schilling <martin@mutz.it>
2014-03-28 11:15:11 +0100

    Added Documentation to Wrapper.cs

--- dottube/Wrapper.cs
+++ dottube/Wrapper.cs
@@ -6,23 +6,35 @@
 
 namespace Dottube
 {
-    using log4net;
     using System.Diagnostics;
     using System.Windows.Forms;
+    using log4net;
 
     /// <summary>
     /// Class Wrapper
     /// </summary>
     class Wrapper
     {
+        /// <summary>
+        /// log4net init
+        /// </summary>
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        /// <summary>
+        /// Update youtube-dl.exe
+        /// </summary>
         public static void UpdateExe()
         {
             string result = YoutubeDl("--update", null);
             MessageBox.Show(result);
         }
 
+        /// <summary>
+        /// Calls youtube-dl.exe with the Parameters defined by <paramref name="strParam"/> on the <paramref name="strURL"/>
+        /// </summary>
+        /// <param name="strParam">Paramters defined by youtube-dl.exe</param>
+        /// <param name="strURL">the Streaming Service URL</param>
+        /// <returns>Console Output of youtube-dl.exe</returns>
         public static string YoutubeDl(string strParam, string strURL)
         {
             Process youtubedl = new Process();
@@ -30,7 +42,7 @@ namespace Dottube
             youtubedl.StartInfo.UseShellExecute = false;
             youtubedl.StartInfo.RedirectStandardOutput = true;  // Redirect the StandardOutput
             youtubedl.StartInfo.FileName = "youtube-dl.exe";
-            youtubedl.StartInfo.Arguments = strParam +" "+ strURL;
+            youtubedl.StartInfo.Arguments = strParam + " " + strURL;
             log.Debug(youtubedl.StartInfo.Arguments.ToString());
             youtubedl.Start();
 
@@ -39,6 +51,5 @@ namespace Dottube
             log.Debug(strOutput);
             return strOutput;
         }
-
     }
 }
